dc002f32c13fa9aba9d888169c8f81dd3aa7df46
Mstolarek <50068721+Mstolarek@users.noreply.github.com>
2021-10-24 14:07:03 +0200

    Implementacja useReducera

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,17 +1,37 @@
-import React, {useState} from 'react';
+import React, {useReducer, useState} from 'react';
 import {View, StyleSheet, Text} from 'react-native';
 import {TextInput} from 'react-native-gesture-handler';
 
 const Searchbar = () => {
-  const [input, setInput] = useState('');
+  const initialState = {input: ''};
+  const reducer = (state: any, action: any) => {
+    switch (action.type) {
+      case 'INPUT':
+        return {state: {input: action.payload}};
+      default:
+        return state;
+    }
+  };
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <View style={styles.container}>
       <TextInput
         style={styles.input}
-        onChangeText={setInput}
+        onChangeText={(payload) => dispatch({type: 'INPUT', payload: payload})}
         placeholder={'Search'}
       />
+      {console.log(state)}
+      <Text
+        style={{
+          position: 'absolute',
+          top: 100,
+          left: 50,
+          fontSize: 16,
+          color: 'black',
+        }}>
+        {state.input}
+      </Text>
     </View>
   );
 };
